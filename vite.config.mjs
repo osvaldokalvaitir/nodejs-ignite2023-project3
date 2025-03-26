05db5a6403ee9e0e7e670733dead04e85e51fecd
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2025-03-25 22:37:04 -0300

    Criando test environment

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,5 +8,24 @@ export default defineConfig({
     coverage: {
       all: false,
     },
+    dir: 'src',
+    workspace: [
+      {
+        extends: true,
+        test: {
+          dir: 'src/http/controllers',
+          name: 'e2e',
+          environment: './prisma/vitest-environments/prisma.ts',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          dir: 'src/use-cases',
+          environment: 'node',
+          name: 'unit',
+        },
+      },
+    ],
   },
 })
