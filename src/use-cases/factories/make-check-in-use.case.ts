4ed91fc6d678cad54a4e807de8dabafd0062be14
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2025-03-29 17:16:11 -0300

    Testes E2E de rotas de academias

--- src/use-cases/factories/make-check-in-use.case.ts
+++ src/use-cases/factories/make-check-in-use.case.ts
@@ -1,5 +1,5 @@
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
-import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-respository'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
 import { CheckInUseCase } from '../check-in'
 
 export function makeCheckInUseCase() {
--- src/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
+++ src/use-cases/factories/make-fetch-nearby-gyms-use-case.ts
@@ -1,4 +1,4 @@
-import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-respository'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
 import { FetchNearbyGymsUseCase } from '../fetch-nearby-gyms'
 
 export function makeFetchNearbyGymsUseCase() {
